11ab97ad600fb69ae4a2a65587db91c8b219a96e
Kamil Antonczak <kamil.m.antonczak@gmail.com>
2017-07-12 14:18:46 +0200

    travis

--- app1/rundocker.sh
+++ app1/rundocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i --net=demo-app --ip 172.18.0.2 -p 127.0.0.1:8080:8080 -t app1 /bin/bash
+docker run --net=demo-app --ip 172.18.0.2 -p 127.0.0.1:8080:8080 app1 /bin/bash
--- app2/rundocker.sh
+++ app2/rundocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -i --net=demo-app --ip 172.18.0.3 -p 8081:8081 -t app2 /bin/bash
+docker run --net=demo-app --ip 172.18.0.3 -p 8081:8081 app2 /bin/bash
